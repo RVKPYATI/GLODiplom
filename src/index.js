a4fbe5501bac5f2c90f63a68a6ec3e44da31df1b
Vasiliy Rusanov <rvk1986@mail.ru>
2022-12-13 19:21:50 +0300

    finish accordeon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { modal } from './modules/modal';
 import { scrollBy } from './modules/scroll';
 import { sliders } from './modules/sliders';
+import { accordeon } from './modules/accordeon';
 
 modal();
 scrollBy();
 sliders();
+accordeon();
